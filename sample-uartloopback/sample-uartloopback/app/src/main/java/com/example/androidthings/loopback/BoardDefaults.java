1884dee914a93dcf865f155d471511c3d65a0d53
mnaganur <mandeepnaganur92@gmail.com>
2018-05-23 20:49:33 -0500

    UART loop

---
+++ sample-uartloopback/sample-uartloopback/app/src/main/java/com/example/androidthings/loopback/BoardDefaults.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.loopback;
+
+import android.os.Build;
+@SuppressWarnings("WeakerAccess")
+public class BoardDefaults {
+    private static final String DEVICE_EDISON = "edison";
+    private static final String DEVICE_JOULE = "joule";
+    private static final String DEVICE_RPI3 = "rpi3";
+    private static final String DEVICE_PICO = "imx6ul_pico";
+    private static final String DEVICE_VVDN = "imx6ul_iopb";
+
+    /**
+     * Return the UART for loopback.
+     */
+    public static String getUartName() {
+        switch (Build.DEVICE) {
+            case DEVICE_EDISON:
+                return "UART1";
+            case DEVICE_JOULE:
+                return "UART1";
+            case DEVICE_RPI3:
+                return "UART0";
+            case DEVICE_PICO:
+                return "UART3";
+            case DEVICE_VVDN:
+                return "UART2";
+            default:
+                throw new IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE);
+        }
+    }
+}
---
+++ sample-uartloopback/test/app/src/main/java/com/test/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.test
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import com.google.android.things.pio.PeripheralManagerService
+import com.google.android.things.pio.UartDevice
+import java.io.IOException
+import java.nio.charset.Charset
+
+class MainActivity : Activity(), OnSeekBarChangeListener  {
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    lateinit var redLabelView : TextView
+    lateinit var greenLabelView : TextView
+    lateinit var blueLabelView : TextView
+
+    lateinit var redBarView : SeekBar
+    lateinit var greenBarView : SeekBar
+    lateinit var blueBarView : SeekBar
+
+    lateinit var device : UartDevice
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        redLabelView = findViewById(R.id.red_label) as TextView
+        greenLabelView = findViewById(R.id.green_label) as TextView
+        blueLabelView = findViewById(R.id.blue_label) as TextView
+
+        redBarView = findViewById(R.id.red) as SeekBar
+        greenBarView = findViewById(R.id.green) as SeekBar
+        blueBarView = findViewById(R.id.blue) as SeekBar
+
+        redBarView.max = 255
+        greenBarView.max = 255
+        blueBarView.max = 255
+
+        redBarView.setOnSeekBarChangeListener(this)
+        greenBarView.setOnSeekBarChangeListener(this)
+        blueBarView.setOnSeekBarChangeListener(this)
+
+        val manager = PeripheralManagerService()
+        try {
+            Log.i(TAG, "UART: " + manager.uartDeviceList)
+
+            device = manager.openUartDevice("UART0")
+
+            device.setBaudrate(9600)
+            device.setDataSize(8)
+            device.setParity(UartDevice.PARITY_NONE)
+            device.setStopBits(1)
+
+            showColor(0, 0, 0)
+        } catch (ex: IOException) {
+            Log.w(TAG, "Error in onCreate", ex)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            device.close()
+        } catch (ex: IOException) {
+            Log.w(TAG, "Error in onDestroy", ex)
+        }
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        val red = redBarView.progress
+        val green = greenBarView.progress
+        val blue = blueBarView.progress
+
+        showColor(red, green, blue)
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        // Don't care
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        // Don't care
+    }
+
+    fun showColor(red: Int, green: Int, blue: Int) {
+        redLabelView.setText("Red ($red)")
+        greenLabelView.setText("Green ($green)")
+        blueLabelView.setText("Blue ($blue)")
+
+        val serialMessage = "$red $green $blue\n"
+        val bytes = serialMessage.toByteArray(Charset.forName("ASCII"))
+        device.write(bytes, bytes.size)
+    }
+}
